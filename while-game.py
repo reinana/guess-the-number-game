b4a729cdaab93d1f2e8ec357774fafda3637f60a
maronana <reina.ciel.0510@gmail.com>
2023-01-02 23:59:50 +0900

    fix while-game.py

--- while-game.py
+++ while-game.py
@@ -1,21 +1,28 @@
 import sys
 import random
 
-print("Guess the number from 1 - 10!")
-min_number = int(input("最小値を入力してください \n"))
-max_number = int(input("最大値を入力してください \n"))
-if min_number >= max_number:
+print("Guess the number!")
+try:
+    n = int(input("最小値を入力してください \n"))
+    m = int(input("最大値を入力してください \n"))
+except ValueError:
+    print("数字を入力してください")
+    sys.exit()
+
+
+if n >= m:
     print("最小値は最大値より小さくしてください")
-
-
-
 else:
-    guess_number = input("予想値を入力してください \n" )
-    random_number = random.randrange(min_number, max_number, 1)
-    while(random_number != int(guess_number)) :
-        print("Wrong")
-        guess_number = input()
-        if(guess_number == ''): 
-            print("Input your guess number.")
-            guess_number = input()
+    random_number = random.randrange(n, m, 1)
+    guess_number = 0
+    while(random_number != guess_number):
+        try:
+            guess_number = int(input("予想値を入力してください\n"))
+        except ValueError:
+            print("数字を入力してください")
+            sys.exit()
+        if random_number != guess_number :
+            print("Wrong")
+
     print("Collect!")
+
